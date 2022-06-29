07f67d8a2887f4d6631ee940d19223680143d110
guohai <guohai@gyyx.cn>
2022-06-29 18:54:15 +0800

    init project

---
+++ src/main/java/org/guohai/iot/IotApplication.java
@@ -0,0 +1,17 @@
+package org.guohai.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 引导程序
+ * @author guohai
+ */
+@SpringBootApplication
+public class IotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotApplication.class, args);
+	}
+
+}
